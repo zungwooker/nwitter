f73c7aa41a93b8ff5d2d8de32445d50fef272b17
zungwooker <zungwooker@gmail.com>
2021-08-29 17:42:52 +0900

    2장 완료

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtaB4E6-hMg5ekrT95NmXE24xjir614nc",
+  authDomain: "nwitter-66e39.firebaseapp.com",
+  projectId: "nwitter-66e39", 
+  storageBucket: "nwitter-66e39.appspot.com",
+  messagingSenderId: "981711754228",
+  appId: "1:981711754228:web:ed0a1dff443e7941031fa1"
+};
+
+export default firebase.initializeApp(firebaseConfig);
