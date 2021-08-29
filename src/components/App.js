cb0141db8f59227f68af555fad50210010edaca7
zungwooker <zungwooker@gmail.com>
2021-08-29 22:08:12 +0900

    3장 완료

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import AppRouter from "./Router";
+
+function App() {
+  return <AppRouter />;
+}
+
+export default App;
+
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtaB4E6-hMg5ekrT95NmXE24xjir614nc",
-  authDomain: "nwitter-66e39.firebaseapp.com",
-  projectId: "nwitter-66e39", 
-  storageBucket: "nwitter-66e39.appspot.com",
-  messagingSenderId: "981711754228",
-  appId: "1:981711754228:web:ed0a1dff443e7941031fa1"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID, 
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
